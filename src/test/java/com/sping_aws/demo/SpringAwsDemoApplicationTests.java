976bb022b3555d0b15e8406126ea9d2c815faaff
Gaurav Kumar <gauravsrivastav04@gmail.com>
2021-09-30 22:18:34 +0530

    First commit

---
+++ src/test/java/com/sping_aws/demo/SpringAwsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sping_aws.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
